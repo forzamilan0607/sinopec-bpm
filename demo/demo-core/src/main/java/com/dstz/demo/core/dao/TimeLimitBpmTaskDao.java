f56abff61356941c25ee7b558a4688416b630c41
zhong.ding <zhong.ding@vtradex.com>
2019-09-23 16:59:44 +0800

    待办列表新增是否延期列

--- demo/demo-core/src/main/java/com/dstz/demo/core/dao/TimeLimitBpmTaskDao.java
+++ demo/demo-core/src/main/java/com/dstz/demo/core/dao/TimeLimitBpmTaskDao.java
@@ -3,6 +3,7 @@ package com.dstz.demo.core.dao;
 import com.dstz.base.dao.BaseDao;
 import com.dstz.bpm.core.model.BpmTask;
 import com.dstz.demo.core.model.TimeLimit;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Map;
@@ -11,9 +12,14 @@ import java.util.Map;
 public interface TimeLimitBpmTaskDao extends BaseDao<String, TimeLimit> {
 
     void saveTimeLimitData(TimeLimit timeLimitData);
+
     void updateTimeLimitData(TimeLimit timeLimitData);
+
     void updateDelayTask(TimeLimit timeLimitData);
+
     TimeLimit getTimeLimitData(TimeLimit timeLimitData);
+
     List<Map<String,Object>> getUserTodoData(String userId);
-    List<TimeLimit> getTimeLimitList(List<String> taskIds);
+
+    List<TimeLimit> getTimeLimitList(@Param("taskIds") List<String> taskIds);
 }
--- demo/demo-core/src/main/java/com/dstz/demo/rest/controller/TimeLimitBpmTaskHandler.java
+++ demo/demo-core/src/main/java/com/dstz/demo/rest/controller/TimeLimitBpmTaskHandler.java
@@ -3,7 +3,7 @@ package com.dstz.demo.rest.controller;
 import com.dstz.agilebpm.component.j2cache.SpringContextUtils;
 import com.dstz.base.api.query.QueryFilter;
 import com.dstz.base.api.response.impl.ResultMsg;
-import com.dstz.base.db.model.query.DefaultQueryFilter;
+import com.dstz.base.db.model.page.PageResult;
 import com.dstz.base.rest.ControllerTools;
 import com.dstz.base.rest.util.RequestUtil;
 import com.dstz.bpm.api.model.inst.IBpmInstance;
@@ -14,9 +14,10 @@ import com.dstz.demo.core.manager.TimeLimitBpmTaskManager;
 import com.dstz.demo.core.model.BpmTaskNew;
 import com.dstz.demo.core.model.TimeLimit;
 import com.dstz.sys.util.ContextUtil;
+import com.github.pagehelper.Page;
+import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,40 +36,44 @@ public class TimeLimitBpmTaskHandler extends ControllerTools {
     @Autowired
     private BpmTaskManager bpmTaskManager;
 
-    @RequestMapping("/bpm/task/todoTaskList")
-    public List<BpmTaskNew> popupMsg(HttpServletRequest request, HttpServletResponse response) throws Exception {
-//        String userId = RequestUtil.getRQString(request, "userId");
-//        timeLimitBpmTaskManager.getUserTodoData(userId);
-        List<BpmTaskNew> bpmTaskNewList=new ArrayList<>();
-        QueryFilter queryFilter =new DefaultQueryFilter();
-        List<BpmTask> listTodoTask = bpmTaskManager.getTodoList(ContextUtil.getCurrentUserId(),queryFilter);
-        System.out.print(1);
-        List<TimeLimit> listTimeLimit =timeLimitBpmTaskManager.getTimeLimitList(listTodoTask);
-        if(bpmTaskNewList.size()>0&&listTimeLimit!=null&&listTimeLimit.size()>0){
-            for (BpmTask param: listTodoTask) {
-                for (TimeLimit param2:listTimeLimit) {
-                    if(param.getId().equals(param2.getTaskId())){
-                        BpmTaskNew bpmTaskNew =new BpmTaskNew();
-                        BeanUtils.copyProperties(param,bpmTaskNew);
-                        bpmTaskNew.setDelayFlag(param2.getTaskEndTime().before(new Date()));
-                        break;
-                    }
+    @RequestMapping("/custom/my/todoTaskList")
+    public PageResult popupMsg(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        List<BpmTaskNew> bpmTaskNewList = new Page<>();
+        QueryFilter queryFilter = super.getQueryFilter(request);
+        List<BpmTask> listTodoTask = bpmTaskManager.getTodoList(ContextUtil.getCurrentUserId(), queryFilter);
+        if (CollectionUtils.isNotEmpty(listTodoTask)) {
+            List<TimeLimit> listTimeLimit = timeLimitBpmTaskManager.getTimeLimitList(listTodoTask);
+            for (BpmTask task : listTodoTask) {
+                if (!this.addTaskNew(bpmTaskNewList, listTimeLimit, task)) {
+                    bpmTaskNewList.add(BpmTaskNew.build(task));
                 }
-
             }
         }
-        return bpmTaskNewList;
+        Page<BpmTaskNew> pageList = (Page) bpmTaskNewList;
+        return new PageResult(pageList);
+    }
+
+    private boolean addTaskNew(List<BpmTaskNew> bpmTaskNewList, List<TimeLimit> listTimeLimit, BpmTask task) {
+        for (TimeLimit timeLimit : listTimeLimit) {
+            if (task.getId().equals(timeLimit.getTaskId())) {
+                BpmTaskNew taskNew = BpmTaskNew.build(task);
+                taskNew.setDelayFlag(timeLimit.getTaskEndTime().before(new Date()));
+                bpmTaskNewList.add(taskNew);
+                return true;
+            }
+        }
+        return false;
     }
 
     @RequestMapping("/bpm/task/delayTask")
     public ResultMsg<String> delayTask(HttpServletRequest request, HttpServletResponse response) throws Exception {
 //        String oldPassWorld = RequestUtil.getRQString(request, "oldPassword","旧密码必填");
 //        String newPassword = RequestUtil.getRQString(request, "newPassword","新密码必填");
-        TimeLimit timeLimitDate =new TimeLimit();
+        TimeLimit timeLimitDate = new TimeLimit();
         timeLimitDate.setTaskId(RequestUtil.getRQString(request, "taskId"));
-        TimeLimit resultTimeLimit =timeLimitBpmTaskManager.getTimeLimitData(timeLimitDate);
+        TimeLimit resultTimeLimit = timeLimitBpmTaskManager.getTimeLimitData(timeLimitDate);
         timeLimitDate.setDelayReason(RequestUtil.getRQString(request, "reason"));
-        Date taskEndTime =getTaskEndTime(resultTimeLimit.getTaskEndTime(),RequestUtil.getRQString(request, "delayTime"));
+        Date taskEndTime = getTaskEndTime(resultTimeLimit.getTaskEndTime(), RequestUtil.getRQString(request, "delayTime"));
         timeLimitDate.setIsDelay(1);
         timeLimitDate.setDelayTime(RequestUtil.getRQString(request, "delayTime"));
         timeLimitDate.setTaskEndTime(taskEndTime);
@@ -80,20 +85,21 @@ public class TimeLimitBpmTaskHandler extends ControllerTools {
 
     /**
      * 流程前置事件  配置时间限制
+     *
      * @param bpmInstance
      * @param bpmTask
      * @param timeLimitStr
      * @return
      */
-    public static String  getTimeLimit(IBpmInstance bpmInstance, IBpmTask bpmTask, String timeLimitStr){
+    public static String getTimeLimit(IBpmInstance bpmInstance, IBpmTask bpmTask, String timeLimitStr) {
         //得到当前流程的当前环节的时间限制值
         TimeLimitBpmTaskManager staticTimeLimitBpmTaskManager = SpringContextUtils.getBean(TimeLimitBpmTaskManager.class);
-        System.out.print("timeLimitStr="+timeLimitStr);
-        System.out.print("bpmInstance="+bpmInstance.getActInstId());
+        System.out.print("timeLimitStr=" + timeLimitStr);
+        System.out.print("bpmInstance=" + bpmInstance.getActInstId());
 //        System.out.print("bpmTask="+bpmTask.);
-        Date createTime=new Date();
-        Date taskEndTime =getTaskEndTime(createTime,timeLimitStr);
-        TimeLimit timeLimitDate =new TimeLimit();
+        Date createTime = new Date();
+        Date taskEndTime = getTaskEndTime(createTime, timeLimitStr);
+        TimeLimit timeLimitDate = new TimeLimit();
         timeLimitDate.setId(UUID.randomUUID().toString());
         timeLimitDate.setInstId(bpmInstance.getId());
         timeLimitDate.setTaskId(bpmTask.getId());
@@ -101,37 +107,39 @@ public class TimeLimitBpmTaskHandler extends ControllerTools {
         timeLimitDate.setCreateTime(createTime);
         timeLimitDate.setTaskStartTime(createTime);
         timeLimitDate.setTaskEndTime(taskEndTime);
-        System.out.print("taskEndTime="+taskEndTime.toString());
+        System.out.print("taskEndTime=" + taskEndTime.toString());
         staticTimeLimitBpmTaskManager.saveTimeLimitData(timeLimitDate);
-        
-       return timeLimitStr;
+
+        return timeLimitStr;
     }
-    public static Date getTaskEndTime(Date createTime,String timeLimitStr){
-        Date taskEndDate =null;
-        if(timeLimitStr!=null&&timeLimitStr.contains("天")){
-            int day =Integer.parseInt(timeLimitStr.substring(0,timeLimitStr.indexOf("天")));
+
+    public static Date getTaskEndTime(Date createTime, String timeLimitStr) {
+        Date taskEndDate = null;
+        if (timeLimitStr != null && timeLimitStr.contains("天")) {
+            int day = Integer.parseInt(timeLimitStr.substring(0, timeLimitStr.indexOf("天")));
 //            int hour =Integer.parseInt(timeLimitStr.substring(timeLimitStr.indexOf("天"),timeLimitStr.indexOf("时")));
-            Calendar calendar=Calendar.getInstance();
+            Calendar calendar = Calendar.getInstance();
             calendar.setTime(createTime);
             System.out.println(calendar.get(Calendar.DAY_OF_MONTH));//今天的日期
-            calendar.set(Calendar.DAY_OF_MONTH,calendar.get(Calendar.DAY_OF_MONTH)+day);//让日期加1
+            calendar.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH) + day);//让日期加1
             System.out.println(calendar.get(Calendar.DATE));//加1之后的日期Top
-             taskEndDate=calendar.getTime();
+            taskEndDate = calendar.getTime();
         }
         return taskEndDate;
     }
 
     /**
      * 流程后置事件  更新流程节点处理时间
+     *
      * @param bpmInstance
      * @param bpmTask
      */
-    public static void dealProcessTimeLimit(IBpmInstance bpmInstance, IBpmTask bpmTask){
+    public static void dealProcessTimeLimit(IBpmInstance bpmInstance, IBpmTask bpmTask) {
         TimeLimitBpmTaskManager staticTimeLimitBpmTaskManager = SpringContextUtils.getBean(TimeLimitBpmTaskManager.class);
-        TimeLimit timeLimitDate =new TimeLimit();
+        TimeLimit timeLimitDate = new TimeLimit();
         timeLimitDate.setInstId(bpmInstance.getId());
         timeLimitDate.setTaskId(bpmTask.getId());
-        TimeLimit resultTimeLimit =staticTimeLimitBpmTaskManager.getTimeLimitData(timeLimitDate);
+        TimeLimit resultTimeLimit = staticTimeLimitBpmTaskManager.getTimeLimitData(timeLimitDate);
         timeLimitDate.setId(resultTimeLimit.getId());
         timeLimitDate.setTaskDealTime(new Date());
         staticTimeLimitBpmTaskManager.updateTimeLimitData(timeLimitDate);
